f0e24707c1e6b9fe73a9881a599546959d7e0b53
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-05 20:28:40 -0300

    versão final

--- App.js
+++ App.js
@@ -1,112 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Text, 
+  TextInput,
+  TouchableOpacity, 
+  View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type Props = {};
+export default class App extends Component<Props> {
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  constructor(props){
+    super(props)
+    this.state = {altura:0, massa:0, resultado:0, resultadoText:""}
+    this.calcular = this.calcular.bind(this)
+  } 
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    calcular() {
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+      let imc = this.state.massa / (this.state.altura * this.state.altura)
+      
+      let s = this.state
+      s.resultado = imc
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      if(s.resultado < 16) {
+        s.resultadoText = "Magreza Grave"
+
+      } else if(s.resultado < 17) {
+        s.resultadoText = "Magreza Moderada"
+
+      } else if(s.resultado < 18.5) {
+        s,resultadoText = "Magreza Leve"
+
+      } else if(s.resultado < 25) {
+        s.resultadoText = "Saudável"
+
+      } else if(s.resultado < 30) {
+        s.resultadoText = "Sobrepeso"
+
+      } else if(s.resultado < 35) {
+        s.resultadoText = "Obesidade Grau I"
+
+      } else if(s.resultado < 40) {
+        s.resultadoText = "Obesidade Grau II (severa)"
+
+      } else {
+        s.resultadoText = "Obesidade Grau III (móbida)"
+
+      }
+      this.setState(s)
+    }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.entradas}>
+          <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input} onChangeText={(massa)=>{this.setState({massa})}}/>  
+          <TextInput placeholder="Altura" keyboardType="numeric"style={styles.input} onChangeText={(altura)=>{this.setState({altura})}}/>  
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+      <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+      <Text style={[styles.resultado, {fontSize:35} ]}>{this.state.resultadoText}</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,       
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  entradas:{
+    flexDirection: 'row',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  input:{
+    height:80,
+    textAlign:"center",
+    width:"50%",
+    fontSize:50,
+    marginTop:24,
+    color:"lightgray"
   },
-  highlight: {
-    fontWeight: '700',
+  button:{
+    backgroundColor:"#89ffa5"
   },
+  buttonText:{
+    alignSelf: 'center',
+    padding: 30,
+    fontSize:25,
+    color:"#6dc4a4",
+    fontWeight: 'bold',
+  },
+  resultado:{
+    alignSelf:"center",
+    color:"lightgray",
+    fontSize:65,
+    padding:15
+  }
 });
-
-export default App;
